0159e3d56b2cec95a94bcaf54e66ed0d1c42bf67
John P. McCrae <john@mccr.ae>
2023-12-19 10:31:09 +0000

    Rebuild WASM

--- dmlex-web/dist/dmlex-web-280ed05a3fd2ec4b.js
+++ dmlex-web/dist/dmlex-web-280ed05a3fd2ec4b.js
@@ -713,12 +713,12 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3565 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 1157, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper3297 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 1159, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3796 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1242, __wbg_adapter_27);
+    imports.wbg.__wbindgen_closure_wrapper3528 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1244, __wbg_adapter_27);
         return addHeapObject(ret);
     };
 
@@ -760,7 +760,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('dmlex-web-3951cd3f8bae4033_bg.wasm', import.meta.url);
+        input = new URL('dmlex-web-280ed05a3fd2ec4b_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
